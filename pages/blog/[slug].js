d4b9a887e25ab8684343c5ec8ff9971e59c9d680
joshuajabakumar <joshua.jabakumar@bp.com>
2022-05-19 00:35:14 +0530

    Blog Detail page author tag color change

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -9,7 +9,7 @@ const BlogTags = (props) => {
       <HStack spacing={2} marginTop={props.marginTop} marginBottom={props.marginBottom} justifyContent={"right"}>
         {props.tags.map((tag) => {
           return (
-            <Tag size={'sm'} variant="solid" colorScheme="orange" key={`${Math.random()}`}>
+            <Tag size={'sm'} variant="solid" colorScheme="blackAlpha" key={`${Math.random()}`}>
               {tag}
             </Tag>
           );

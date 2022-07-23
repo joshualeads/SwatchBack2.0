9bdabb9fcbd2808a594033d457921f0255784679
joshuajabakumar <joshua.jabakumar@bp.com>
2022-07-23 12:18:39 +0530

    Progress till 23-July-2022

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
   env: {
     SHOPIFY_STOREFRONT_ACCESSTOKEN:'352c4e85a0242da524eb24ed9cce7c1d',
     SHOPIFY_STORE_DOMAIN:'swatchback.myshopify.com',
-    STRAPI_CMS_DOMAIN:'https://strapi-swatchback-deploy.herokuapp.com',
-    NEXT_PUBLIC_STRAPI_API_URL: 'https://strapi-swatchback-deploy.herokuapp.com'
+    STRAPI_CMS_DOMAIN:'https://oyster-app-vxwdi.ondigitalocean.app',
+    NEXT_PUBLIC_STRAPI_API_URL: 'https://oyster-app-vxwdi.ondigitalocean.app'
   },
   images: {
-    domains: ['cdn.shopify.com','strapi-swatchback-deploy.herokuapp.com']
+    domains: ['cdn.shopify.com','strapi-swatchback-deploy.herokuapp.com','oyster-app-vxwdi.ondigitalocean.app']
   }
 }
 
